9aec3c21561c87d2cfdd2c3426a958423e92188c
guillaume.franguiadakis <guillaume.franguiadakis@eemi.com>
2023-01-03 19:54:00 +0100

    create paddle

---
+++ script.js
@@ -0,0 +1,69 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Paddle {
+    constructor({position}){
+        this.position = position;
+        this.velocity = {
+            x:0,
+            y:0
+        }
+        this.width = 10;
+        this.height = 100;
+    }
+
+    draw(){
+        c.fillStyle = 'white';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(){
+        this.draw();
+
+        if(this.position.y < 0)
+        this.position.y += this.velocity.y;
+    }
+}
+
+
+const paddle1 = new Paddle({position:{
+    x: 10,
+    y: 100,
+},
+})
+
+const paddle2 = new Paddle({position:{
+    x: canvas.width - 10 * 2,
+    y: 100,
+},
+})
+
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.fillStyle = 'black'
+    c.fillRect(0,0,canvas.width, canvas.height);
+    paddle1.update()
+    paddle2.update()
+}
+
+animate();
+
+addEventListener('keydown', (event) => {
+    switch(event.key){
+        case 'w':
+
+            paddle1.velocity.y = -1;
+
+        break
+        case 's':
+
+            paddle1.velocity.y = 1;
+
+        break
+    }
+})
